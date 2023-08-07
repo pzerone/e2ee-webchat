a9fda734520181088cec56be9ce11107469db17c
Sreejith IV <46400271+pzerone@users.noreply.github.com>
2023-08-07 14:25:14 +0530

    Removed dummy chat page js code

--- src/static/script.js
+++ src/static/script.js
@@ -46,51 +46,55 @@ function decryptMessage(encryptedMessage, privateKey) {
     var decryptedMessage = crypt.decrypt(encryptedMessage);
     return decryptedMessage;
 }
+
+
+
+
 // Unhides the chatbox when the connect button is pressed and only if connect_to form is filled
-document.getElementById('connect').onclick = function() {
-    if (document.getElementById('connect_to').value === "") {
-        alert("Please enter a username to connect")
-    } else if (document.getElementById('connect_to').value != "") {
-        const chatbox = document.getElementById('chatbox');
-        chatbox.style.display = 'block';
-        const sendbox = document.getElementById('sendbox');
-        sendbox.style.display = 'block';
-        getkey(document.getElementById('connect_to').value)
-    };
-    document.getElementById('send').onclick = function() {
-        var recipient = document.getElementById('connect_to').value;
-        var private_message = document.getElementById('message').value;
-        var recipientPublicKey = localStorage.getItem(recipient)
-        var encryptedMessage = encryptMessage(private_message, recipientPublicKey);
-        socket.emit('private_message', {
-            'recipient': recipient,
-            'message': encryptedMessage
-        });
-        let you = "You: "
-        let finalMessage = you.concat(private_message)
-        let ul = document.getElementById('chat-messages');
-        let li = document.createElement('li');
-        li.appendChild(document.createTextNode(finalMessage));
-        ul.appendChild(li);
-        ul.scrollTop = ul.scrollHeight;
-        document.getElementById('message').value = ""
-    };
-    socket.on('new_mesage', function(payload) {
-        const chatbox = document.getElementById('chatbox');
-        chatbox.style.display = 'block';
-        const sendbox = document.getElementById('sendbox');
-        sendbox.style.display = 'block';
-        document.getElementById('connect_to').value = payload['sender']
-        let ul = document.getElementById('chat-messages');
-        let li = document.createElement('li');
-        let message = payload['message']
-        // decrypt message here
-        var privateKey = localStorage.getItem("private_key");
-        var decryptedMessage = decryptMessage(message, privateKey);
-        let sender = payload['sender']
-        let finalMessage = sender.concat(": ", decryptedMessage)
-        li.appendChild(document.createTextNode(finalMessage));
-        ul.appendChild(li);
-        ul.scrollTop = ul.scrollHeight;
-    });
-};
+// document.getElementById('connect').onclick = function() {
+//     if (document.getElementById('connect_to').value === "") {
+//         alert("Please enter a username to connect")
+//     } else if (document.getElementById('connect_to').value != "") {
+//         const chatbox = document.getElementById('chatbox');
+//         chatbox.style.display = 'block';
+//         const sendbox = document.getElementById('sendbox');
+//         sendbox.style.display = 'block';
+//         getkey(document.getElementById('connect_to').value)
+//     };
+//     document.getElementById('send').onclick = function() {
+//         var recipient = document.getElementById('connect_to').value;
+//         var private_message = document.getElementById('message').value;
+//         var recipientPublicKey = localStorage.getItem(recipient)
+//         var encryptedMessage = encryptMessage(private_message, recipientPublicKey);
+//         socket.emit('private_message', {
+//             'recipient': recipient,
+//             'message': encryptedMessage
+//         });
+//         let you = "You: "
+//         let finalMessage = you.concat(private_message)
+//         let ul = document.getElementById('chat-messages');
+//         let li = document.createElement('li');
+//         li.appendChild(document.createTextNode(finalMessage));
+//         ul.appendChild(li);
+//         ul.scrollTop = ul.scrollHeight;
+//         document.getElementById('message').value = ""
+//     };
+//     socket.on('new_mesage', function(payload) {
+//         const chatbox = document.getElementById('chatbox');
+//         chatbox.style.display = 'block';
+//         const sendbox = document.getElementById('sendbox');
+//         sendbox.style.display = 'block';
+//         document.getElementById('connect_to').value = payload['sender']
+//         let ul = document.getElementById('chat-messages');
+//         let li = document.createElement('li');
+//         let message = payload['message']
+//         // decrypt message here
+//         var privateKey = localStorage.getItem("private_key");
+//         var decryptedMessage = decryptMessage(message, privateKey);
+//         let sender = payload['sender']
+//         let finalMessage = sender.concat(": ", decryptedMessage)
+//         li.appendChild(document.createTextNode(finalMessage));
+//         ul.appendChild(li);
+//         ul.scrollTop = ul.scrollHeight;
+//     });
+// };
